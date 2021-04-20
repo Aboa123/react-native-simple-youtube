8c1bdb4db7c110532a7a0339f89f19722ecc79ba
Aboa123 <wlxm9999@gmail.com>
2021-04-20 21:30:15 +0900

    first comimt

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import { WebView } from 'react-native-webview';
+
+const App = (props) => {
+  const [play, setPlay] = useState(props.useThumbnail);
+
+  return (
+      <View
+      style={[{
+        height: props.height,
+      }, props.style]}>
+        {
+          play ? 
+          <TouchableOpacity
+            onPress={() => setPlay(false)}
+            style={{ flex: 1 }}>
+            <Image
+              style={{ flex: 1 }}
+              resizeMode={props.thumbnailResize}
+              source={{
+                uri: `http://img.youtube.com/vi/${props.videoId}/0.jpg`,
+              }}
+            />
+            <View
+              style={[
+                StyleSheet.absoluteFill,
+                {
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0,0,0,0.5)',
+                },
+              ]}>
+              <Image
+                source={require('./assets/youtube_icon.png')}
+                style={{ width: 70, height: 70 }}
+                resizeMode="contain"
+              />
+            </View>
+          </TouchableOpacity>
+          :
+          <WebView
+            source={{
+              html: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${props.videoId}?rel=0" frameborder="0"></iframe>`,
+              baseUrl: 'http://localhost',
+            }}
+          />
+        }
+      </View>
+  );
+};
+
+App.defaultProps = {
+  videoId: "gOlwjwV9vik",
+  useThumbnail: true,
+  thumbnailResize: "cover",
+  height: "100%",
+  styles: null
+}
+
+export default App;
